4df8f4fab3b26d68dc4c0bcdb4d25c2760ffc1d5
Sehnryr <sehnryr@gmail.com>
2019-10-24 19:54:42 +0200

    Addings the first test texture
    & reorganizing the files

--- main/java/com/enaerys/Main.java
+++ main/java/com/enaerys/Main.java
@@ -3,9 +3,15 @@ package com.enaerys;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.enaerys.lists.ModItems;
 import com.enaerys.util.Reference;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -34,5 +40,25 @@ public class Main {
 	
 	private void clientRegisteries(final FMLClientSetupEvent event){
 		
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents{
+		
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event) {
+
+			event.getRegistry().registerAll(
+				ModItems.test_item = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("test_item"))
+				//BaseItem.test_item = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(new ResourceLocation(modid, "test_item");
+			);
+			
+			logger.info("Items registered.");
+		}
+		
+		private static ResourceLocation location(String name) {
+			
+			return new ResourceLocation(modid, name);
+		}
 	}
 }
